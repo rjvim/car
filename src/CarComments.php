bfa5881be98ef02d7bfadf401fc29e1edac07523
Shubham Joshi <shubham@betalectic.com>
2019-04-24 11:20:30 +0530

    added resolving comments functionality

--- src/CarComments.php
+++ src/CarComments.php
@@ -14,7 +14,9 @@ class CarComments
 
     public function getComments($module = NULL, $user = NULL)
     {
-        $comments = DB::table('car_comments')->get();
+        $comments = DB::table('car_comments')
+                        ->whereNull('deleted_at')
+                        ->get();
         
         if($module) {
             $module_id = $module->getKey();
@@ -42,7 +44,7 @@ class CarComments
         $data['module_type'] = get_class($module);
         $data['user_id'] = $user->getKey();
         $data['comment'] = $comment;
-        $data['action_assigned_comment_id'] =  $assignedRecord ? $assignedRecord->id : NULL;
+        // $data['action_assigned_comment_id'] =  $assignedRecord ? $assignedRecord->id : NULL;
 
         $comment = Comment::create($data);
 
@@ -63,5 +65,18 @@ class CarComments
     {
         $comment =  Comment::find($commentId);
         $comment->delete();
+    }
+
+    public function resolveComments($module=NULL)
+    {
+        $module_id = $module->getKey();
+        $module_type = get_class($module);
+        
+        $comments = DB::table('car_comments')
+                        ->where('module_type', $module_type)
+                        ->where('module_id', $module_id)
+                        ->where('is_resolved', FALSE)
+                        ->update(['is_resolved' => true]);
+
     }
 }
